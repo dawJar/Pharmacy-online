e2e8ba3e977ccbf24e05c81fc8d8b4077627b606
dawJar <dawid.jaracz@gmail.com>
2017-02-22 17:31:21 +0100

    styles for total price component

--- src/js/components/TotalPrice.jsx
+++ src/js/components/TotalPrice.jsx
@@ -1,10 +1,14 @@
 import React from 'react';
 
+import '../../sass/componentStyles/totalPrice.scss';
+
 
 const TotalPrice = ({ totalPrice }) => (
-    <div>
-       <p>total: { totalPrice }</p>
-    </div>
+    <p className="total-price-container pull-right">
+        total price: &nbsp;
+        <span className="total-price">{ totalPrice }</span>
+        &nbsp; pln
+    </p>
 );
 
 export default TotalPrice;
