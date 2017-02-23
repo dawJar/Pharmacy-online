a13bb4b77d3ed31656a98ad3d3985b62324482a7
dawJar <dawid.jaracz@gmail.com>
2017-02-23 01:53:55 +0100

    footer complete

--- src/js/components/Footer.jsx
+++ src/js/components/Footer.jsx
@@ -1,21 +1,19 @@
 import React from 'react';
 
 
-const Footer = () => {
+const Footer = () => (
+    <footer className="main-footer">
+        <div className="content-main-footer">
+            <p className="pull-left">
+                @2017&nbsp;&nbsp;|
+                &nbsp;&nbsp;subscribe&nbsp;us
+            </p>
+            <p className="pull-right">
+                created by dejvid
+            </p>
+        </div>
+    </footer>
+);
 
-    return (
-        <footer className="main-footer">
-            <div className="content-main-footer">
-                <p className="pull-left">
-                    @2017&nbsp;&nbsp;|
-                    &nbsp;&nbsp;subscribe&nbsp;us
-                </p>
-                <p className="pull-right">
-                    created by dejvid
-                </p>
-            </div>
-        </footer>
-    );
-}
 
 export default Footer;
