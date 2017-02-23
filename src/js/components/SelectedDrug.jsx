e45add13480f00f89ad208605d933a446fd897af
dawJar <dawid.jaracz@gmail.com>
2017-02-23 21:10:16 +0100

    selected drug comp changes, cleaning

--- src/js/components/SelectedDrug.jsx
+++ src/js/components/SelectedDrug.jsx
@@ -1,15 +1,20 @@
 import React, { PropTypes } from 'react';
 
+import '../../sass/componentStyles/search.scss';
+
 import DrugContainer from '../containers/DrugContainer';
 import Drug from './Drug';
 
 
 const SelectedDrug = ({ idToShow, drugs, ...otherProps }) => (
-      <Drug
-          { ...drugs[idToShow] }
-          { ...otherProps }
-          removeDetails
-      />
+      <div>
+          <Drug
+              { ...drugs[idToShow] }
+              { ...otherProps }
+              removeDetails
+          />
+          <div className="underscore-selected-drug pull-right"></div>
+      </div>
 );
 
 export default SelectedDrug;
