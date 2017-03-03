dcf83137c74680b98fd141d2b3a891f821a5d82a
Dawid Jaracz <Dawid Jaracz>
2017-03-03 17:59:19 +0100

    cleaning

--- src/js/components/Drug/DrugDetailsSelected.jsx
+++ src/js/components/Drug/DrugDetailsSelected.jsx
@@ -7,32 +7,29 @@ const DrugDetailsSelected = ({
     drugCurrency,
     onSale,
     saleTab
-}) => {
-
-    return (
-        <table className="table-details">
-            <tbody>
-                {
-                  (saleTab) ?
-                      <tr>
-                          <th className="table-details-header">category:</th>
-                          <td className="table-details-data">{ drugCategory }</td>
-                      </tr> : null
-                }
-                {
-                  (!saleTab) ?
-                      <tr>
-                          <th className="table-details-header">company:</th>
-                          <td className="table-details-data">{ drugCompany }</td>
-                      </tr> : null
-                }
-                <tr>
-                    <th className="table-details-header">price:</th>
-                    <td className="table-details-data">{ `${drugPrice} ${drugCurrency}` }</td>
-                </tr>
-            </tbody>
-        </table>
-    );
-}
+}) => (
+    <table className="table-details">
+        <tbody>
+            {
+              (saleTab) ?
+                  <tr>
+                      <th className="table-details-header">category:</th>
+                      <td className="table-details-data">{ drugCategory }</td>
+                  </tr> : null
+            }
+            {
+              (!saleTab) ?
+                  <tr>
+                      <th className="table-details-header">company:</th>
+                      <td className="table-details-data">{ drugCompany }</td>
+                  </tr> : null
+            }
+            <tr>
+                <th className="table-details-header">price:</th>
+                <td className="table-details-data">{ `${drugPrice} ${drugCurrency}` }</td>
+            </tr>
+        </tbody>
+    </table>
+);
 
 export default DrugDetailsSelected;
